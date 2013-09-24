d34ce1dac10e1b4b823d834520881231e78715cd
g-nakanishi <red4193@gmail.com>
2013-09-24 22:03:24 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GrowthRythem::Application.config.secret_key_base = 'da4b91ee872aba45af0a49f803b8d6c789a34c3e4fda5fc418bf16400521dd66b4e7b48848cd5a924407630402231eed9983fd1bba9a24abd4b14326b62e1a52'
