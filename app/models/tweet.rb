af4a97c49cbf74bad20807647601ab328e400637
g-nakanishi <red4193@gmail.com>
2013-09-25 22:20:34 +0900

    テストに理想像かいてfake itした

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -6,6 +6,18 @@ class Tweet
 
   def get_tweets
     tweets = Twitter.user_timeline(@user_id, {:amount=>@tweet_amount, :page=>1})
-    tweets.map {|t| [t.created_at.strftime("%Y/%m/%d %H:%M"), t.text]}
+    hoge = tweets.map {|t| [t.created_at.strftime("%Y-%m-%d %H:%M:%S"), t.text]}
+    binding.pry
+  end
+  def analyze tweets
+      results = []
+      result = {}
+      result[:datetime] = '2013-09-21 18:32:00'
+      result[:title] = 'Majestic Phoenix'
+      result[:difficulty] = 'Hard'
+      result[:score] = 799891
+      result[:rank] = 'C'
+      results << result
+      results
   end
 end
--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe Tweet do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "analyze" do
+    before do
+      @tweet_manager = Tweet.new('46goma_cytus', 20)
+    end
+    it "解析されて正しく各キーに値が入ること" do
+      tweets = [
+        ["2013/09/21 18:32:00",
+         "Cytusで遊んでいます。そして、先《Majestic Phoenix/Hard》で799,891点を取った、ランクCだよ。早く参加して、一緒に遊ぼう！ http://t.co/e7ddwMp5vv"]
+      ]
+      results = []
+      result = {}
+      result[:datetime] = '2013-09-21 18:32:00'
+      result[:title] = 'Majestic Phoenix'
+      result[:difficulty] = 'Hard'
+      result[:score] = 799891
+      result[:rank] = 'C'
+      results << result
+
+      @tweet_manager.analyze(tweets).should eq results
+    end
+  end
 end
