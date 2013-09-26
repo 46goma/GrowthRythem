62b61e5e2c27845374c485378fe6d03184e517b1
g-nakanishi <red4193@gmail.com>
2013-09-26 17:10:04 +0900

    特定の書式(cytus仕様)になっていると仮定してパースできるようにした fixed i2

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -6,18 +6,19 @@ class Tweet
 
   def get_tweets
     tweets = Twitter.user_timeline(@user_id, {:amount=>@tweet_amount, :page=>1})
-    hoge = tweets.map {|t| [t.created_at.strftime("%Y-%m-%d %H:%M:%S"), t.text]}
-    binding.pry
+    tweets.map {|t| [t.created_at.strftime("%Y-%m-%d %H:%M:%S"), t.text]}
   end
   def analyze tweets
-      results = []
+    results = tweets.map do |tweet|
       result = {}
-      result[:datetime] = '2013-09-21 18:32:00'
-      result[:title] = 'Majestic Phoenix'
-      result[:difficulty] = 'Hard'
-      result[:score] = 799891
-      result[:rank] = 'C'
-      results << result
-      results
+      parsed_tweet = tweet.last.match(%r{《(.*)/(.*)》で(.*)点を取った、ランク(.*)だ})
+      result[:datetime] = tweet.first
+      result[:title] = parsed_tweet[1]
+      result[:difficulty] = parsed_tweet[2]
+      result[:score] = parsed_tweet[3].gsub(',','').to_i
+      result[:rank] = parsed_tweet[4]
+      result
+    end
+    results
   end
 end
